4751a568fe7e2414ad158688ec37ca2cf7138952
NiazSagor <niazsagor@gmail.com>
2026-02-27 20:28:13 +0600

    Remove debug rendering configuration from main entry point
    - Remove unused `flutter/rendering.dart` import
    - Disable `debugRepaintRainbowEnabled` in the `main` function
    
    Took 10 minutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_ecommerce/data/repositories/auth/auth_repository_remote.dart';
 import 'package:flutter_ecommerce/data/repositories/product/product_repository_remote.dart';
 import 'package:flutter_ecommerce/data/repositories/user/user_repository_remote.dart';
@@ -14,7 +13,6 @@ import 'ui/features/products/view_models/product_list_view_model.dart';
 import 'ui/features/profile/view_models/user_view_model.dart';
 
 void main() {
-  debugRepaintRainbowEnabled = true;
   runApp(const MyApp());
 }
 
